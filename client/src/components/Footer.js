c925c64029ded6b7419d25bd3fabdd8dbf82e010
elizabeth189 <elizabethlopez0108@gmail.com>
2023-04-27 21:07:26 -0400

    updated twitter, about.js, added package.json

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,16 +1,29 @@
-import React from 'react';
-
+import React from "react";
+import { TwitterFollowButton } from "react-twitter-embed";
 const Footer = () => {
-    return (
-    <div className='bg-[#C0504D] h-[200px] flex flex-col justify-center'>
-         <ul className='justify-center md:text-xl'>
-                    <li className='flex justify-center p-2 hover:opacity-70'><a className='ml-1 text-white' href="/">HOME</a></li>
-                    <li className='flex justify-center p-2 hover:opacity-70'><a className='ml-1 text-white' href="#work" >ABOUT ROBBIE</a></li>
-                    <li className='flex justify-center p-2 hover:opacity-70'><a className='ml-1 text-white' href="/">SIGN UP</a></li>
-                    <a className='flex justify-center p-2 hover:opacity-70 ml-1 text-white' href="https://twitter.com/Robbie4Pr3z?ref_src=twsrc%5Etfw"  class="twitter-follow-button"  data-show-count="false">Follow @Robbie4Pr3z</a><script async src="https://platform.twitter.com/widgets.js " charset="utf-8"></script>
-                    {/* <li className='flex justify-center p-2 hover:opacity-70'><a className='ml-1 text-white' href="https://github.com/jakeroth0" target="blank" rel="noopener noreferrer">TWEETS</a></li> */}
-                </ul>
+  return (
+    <div className="bg-[#C0504D] h-[200px] flex flex-col justify-center">
+      <ul className="justify-center md:text-xl">
+        <li className="flex justify-center p-2 hover:opacity-70">
+          <a className="ml-1 text-white" href="/">
+            HOME
+          </a>
+        </li>
+        <li className="flex justify-center p-2 hover:opacity-70">
+          <a className="ml-1 text-white" href="#work">
+            ABOUT ROBBIE
+          </a>
+        </li>
+        <li className="flex justify-center p-2 hover:opacity-70">
+          <a className="ml-1 text-white" href="/">
+            SIGN UP
+          </a>
+        </li>
+        {/* tweets */}
+        <TwitterFollowButton screenName="Robbie4Pr3z" showCount={false} />
+        {/* <li className='flex justify-center p-2 hover:opacity-70'><a className='ml-1 text-white' href="https://github.com/jakeroth0" target="blank" rel="noopener noreferrer">TWEETS</a></li> */}
+      </ul>
     </div>
-
-)}
-export default Footer; 
+  );
+};
+export default Footer;
